08a7bcb35c67948dfa91e4d40ff02f0fe0795f3f
ichikmarev <ichikmarev@gmail.com>
2018-12-24 10:00:24 +0300

    Changes for input and output

--- Main.hs
+++ Main.hs
@@ -12,13 +12,12 @@ import Control.Monad
 import qualified Data.ByteString.Lazy as B
 import Network.HTTP.Conduit (simpleHttp)
 import GHC.Generics
+import Control.Monad (mapM_)
 
-data DataPoint = DataPoint { id :: Int
-                           , description :: String
-                           , icon :: String
+data DataPoint = DataPoint { description :: String
                            } deriving (Show, Generic)
 
-data Temperatures = Temperatures { weather :: [DataPoint]
+data Temperatures = Temperatures { weather :: DataPoint
                                  } deriving (Show, Generic)
 
 instance FromJSON Temperatures
@@ -26,16 +25,19 @@ instance ToJSON Temperatures
 instance FromJSON DataPoint
 instance ToJSON DataPoint
 
-jsonURL :: String
-jsonURL = "http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=e7b9dd9f41c3ac26eae9e94536c8075e"
+jsonURL :: String -> String
+jsonURL q = "http://api.openweathermap.org/data/2.5/weather?q=" ++ q ++ "&units=metric&appid=e7b9dd9f41c3ac26eae9e94536c8075e"
 
-getJSON :: IO B.ByteString
-getJSON = simpleHttp jsonURL
-
+getJSON :: String -> IO B.ByteString
+getJSON town = simpleHttp (jsonURL town)
 
 main :: IO ()
 main = do
- d <- (eitherDecode <$> getJSON) :: IO (Either String Temperatures)
+ putStrLn "Hello! Please insert your town >>> \n "
+ town <- getLine
+ putStrLn ("Your town is: " ++ town ++ "\n")
+ d <- (eitherDecode <$> (getJSON town)) :: IO (Either String Temperatures)
  case d of
-  Left e   -> putStrLn e
-  Right stuff -> print stuff
+  Left e -> putStrLn "Error occured. Try again please"
+  Right stuff -> print ("Weather now is " ++ (description (weather stuff)))
+  -- Right stuff -> print (fmap weather stuff)
