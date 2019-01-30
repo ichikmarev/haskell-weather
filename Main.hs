4bf268f8ebc1945a43c00c4b8000ae17dedcb2f5
Ilya Chikmarev <ichikmarev@gmail.com>
2019-01-30 12:27:44 +0300

    Fixed new JSON structure parsing and added tempeature value

--- Main.hs
+++ Main.hs
@@ -13,17 +13,31 @@ import qualified Data.ByteString.Lazy as B
 import Network.HTTP.Conduit (simpleHttp)
 import GHC.Generics
 import Control.Monad (mapM_)
+import Prelude
 
 data DataPoint = DataPoint { description :: String
                            } deriving (Show, Generic)
 
-data Temperatures = Temperatures { weather :: DataPoint
+data TempPoint = TempPoint { temp :: Float
+                           } deriving (Show, Generic)
+
+data Temperatures = Temperatures { weather :: [DataPoint],
+                                   test :: TempPoint
                                  } deriving (Show, Generic)
 
-instance FromJSON Temperatures
-instance ToJSON Temperatures
+instance FromJSON Temperatures where {
+  parseJSON = withObject "" $ \o ->
+    Temperatures <$> o .: "weather" <*> o .: "main"
+}
+instance ToJSON Temperatures where {
+  toJSON p = object [
+    "weather" .= weather p,
+    "main" .= test p]
+}
 instance FromJSON DataPoint
 instance ToJSON DataPoint
+instance FromJSON TempPoint
+instance ToJSON TempPoint
 
 jsonURL :: String -> String
 jsonURL q = "http://api.openweathermap.org/data/2.5/weather?q=" ++ q ++ "&units=metric&appid=e7b9dd9f41c3ac26eae9e94536c8075e"
@@ -38,6 +52,6 @@ main = do
  putStrLn ("Your town is: " ++ town ++ "\n")
  d <- (eitherDecode <$> (getJSON town)) :: IO (Either String Temperatures)
  case d of
-  Left e -> putStrLn "Error occured. Try again please"
-  Right stuff -> print ("Weather now is " ++ (description (weather stuff)))
+  Left e -> putStrLn e
+  Right stuff -> putStrLn ("Weather now is: \n" ++ (description (Prelude.head(weather stuff))) ++ "\n\nTempeature now is: " ++ show (temp (test stuff)))
   -- Right stuff -> print (fmap weather stuff)
--- dist/build/WeatherApp/autogen/Paths_WeatherApp.hs
+++ dist/build/WeatherApp/autogen/Paths_WeatherApp.hs
@@ -29,12 +29,12 @@ version :: Version
 version = Version [0,1,0,0] []
 bindir, libdir, dynlibdir, datadir, libexecdir, sysconfdir :: FilePath
 
-bindir     = "/Users/thesoulofthetime/.cabal/bin"
-libdir     = "/Users/thesoulofthetime/.cabal/lib/x86_64-osx-ghc-8.4.3/WeatherApp-0.1.0.0-B1qmizdwVzr1lGbsS1P2JV-WeatherApp"
-dynlibdir  = "/Users/thesoulofthetime/.cabal/lib/x86_64-osx-ghc-8.4.3"
-datadir    = "/Users/thesoulofthetime/.cabal/share/x86_64-osx-ghc-8.4.3/WeatherApp-0.1.0.0"
-libexecdir = "/Users/thesoulofthetime/.cabal/libexec/x86_64-osx-ghc-8.4.3/WeatherApp-0.1.0.0"
-sysconfdir = "/Users/thesoulofthetime/.cabal/etc"
+bindir     = "/Users/ichikmarev/Library/Haskell/bin"
+libdir     = "/Users/ichikmarev/Library/Haskell/ghc-8.6.3-x86_64/lib/WeatherApp-0.1.0.0"
+dynlibdir  = "/Users/ichikmarev/Library/Haskell/ghc-8.6.3-x86_64/lib/x86_64-osx-ghc-8.6.3"
+datadir    = "/Users/ichikmarev/Library/Haskell/share/ghc-8.6.3-x86_64/WeatherApp-0.1.0.0"
+libexecdir = "/Users/ichikmarev/Library/Haskell/libexec/x86_64-osx-ghc-8.6.3/WeatherApp-0.1.0.0"
+sysconfdir = "/Users/ichikmarev/Library/Haskell/etc"
 
 getBinDir, getLibDir, getDynLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
 getBinDir = catchIO (getEnv "WeatherApp_bindir") (\_ -> return bindir)
