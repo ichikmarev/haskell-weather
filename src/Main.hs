5688a038138706553afd6c5fe764bdc49dadb0c6
Ilya Chikmarev <ichikmarev@gmail.com>
2019-03-06 08:28:10 +0300

    Error handler

--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,7 @@ import Control.Applicative
 import Control.Monad
 import qualified Data.ByteString.Lazy as B
 import Network.HTTP.Conduit (simpleHttp)
+import Control.Exception as X
 import GHC.Generics
 import Control.Monad (mapM_)
 import Prelude
@@ -43,8 +44,11 @@ instance ToJSON TempPoint
 jsonURL :: String -> String
 jsonURL q = "http://api.openweathermap.org/data/2.5/weather?q=" ++ q ++ "&units=metric&appid=e7b9dd9f41c3ac26eae9e94536c8075e&lang=ru"
 
+statusExceptionHandler ::  SomeException -> IO B.ByteString
+statusExceptionHandler e = (putStrLn "Что-то не так") >> (return B.empty)
+
 getJSON :: String -> IO B.ByteString
-getJSON town = simpleHttp (jsonURL town)
+getJSON town = simpleHttp (jsonURL town) `X.catch` statusExceptionHandler
 
 main :: IO ()
 main = do
@@ -103,4 +107,3 @@ getWeather = do
   Right stuff -> putStrLn ("Сейчас температура в твоем городе " ++
                             show (temp (test stuff)) ++ " градусов Цельсия \nА на улице – " ++
                             (description (Prelude.head(weather stuff))))
-  -- Right stuff -> print (fmap weather stuff)
